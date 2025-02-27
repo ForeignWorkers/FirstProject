69ef1d8b30ffd9092ce0b4b3cc145d0f79ab85a3
Taniliem <plasmar4@gmail.com>
2025-02-27 17:09:19 +0900

    리뷰VO 수정 + 리뷰 매니저
    추가 or 수정사항 있으면 말씀해주세요

---
+++ src/main/java/Managers/ReviewManager.java
@@ -0,0 +1,27 @@
+package Managers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import TESTVO.TestReviewVO;
+
+public class ReviewManager {
+
+	private List<TestReviewVO> reviewList; // 리뷰 목록 저장
+
+    public ReviewManager() {
+        reviewList = new ArrayList<>();
+    }
+
+    // 리뷰 추가
+    public void addReview(String name, String content, double score) {
+        TestReviewVO newReview = new TestReviewVO(name, content, score);
+        reviewList.add(newReview);
+    }
+
+    // 저장된 리뷰 리스트 반환
+    public List<TestReviewVO> getReviews() {
+        return reviewList;
+    }
+	
+}
